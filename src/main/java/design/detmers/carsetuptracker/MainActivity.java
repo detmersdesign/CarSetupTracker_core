6af68db02ddc53c4641a487d15bec6bb0dd3c39b
detmers.design@gmail.com <@miata1WRX>
2016-10-11 16:39:55 -0700

    Initial Commit

--- src/main/java/design/detmers/carsetuptracker/MainActivity.java
+++ src/main/java/design/detmers/carsetuptracker/MainActivity.java
@@ -1,6 +1,9 @@
 package design.detmers.carsetuptracker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,18 +13,55 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     Button buttonOne,buttonTwo,buttonThree;
+    final Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonOne = (Button) findViewById(R.id.session);
-        buttonTwo = (Button) findViewById(R.id.dev_s);
+        buttonOne = (Button) findViewById(R.id.align_set);
+        buttonTwo = (Button) findViewById(R.id.session);
+        buttonThree = (Button) findViewById(R.id.dev_s);
         buttonOne.setOnClickListener(this);
         buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.align_set:
+                //open alignment settings
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set title
+                alertDialogBuilder.setTitle("Your Title");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Click yes to exit!")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, close
+                                // current activity
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+                break;
             case R.id.session:
                 //open session test
                 Intent intent1 = new Intent(this, Session.class);
