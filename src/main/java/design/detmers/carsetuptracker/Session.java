ff2c7ba332474ed2f2207d30428777ad73afde77
detmers.design@gmail.com <@miata1WRX>
2016-10-11 19:09:54 -0700

    Added some portrait/landscape functionality with buttons to Session_Top

--- src/main/java/design/detmers/carsetuptracker/Session.java
+++ src/main/java/design/detmers/carsetuptracker/Session.java
@@ -1,10 +1,11 @@
 package design.detmers.carsetuptracker;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Session extends AppCompatActivity {
+public class Session extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- src/main/java/design/detmers/carsetuptracker/Session_Top.java
+++ src/main/java/design/detmers/carsetuptracker/Session_Top.java
@@ -1,16 +1,60 @@
 package design.detmers.carsetuptracker;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Session_Top extends AppCompatActivity {
-
+//public class Session_Top extends Activity {
+public class Session_Top extends Activity implements View.OnClickListener {
+    Button land_LF,land_LR,land_RF,land_RR;
+    Button vert_LF,vert_LR,vert_RF,vert_RR;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session__top);
 
-        Intent intent = getIntent();
+        //Intent intent = getIntent();
+        if(getResources().getDisplayMetrics().widthPixels>getResources().getDisplayMetrics().
+                heightPixels)
+        {
+            land_LF = (Button) findViewById(R.id.land_buttonLF);
+            land_LR = (Button) findViewById(R.id.land_buttonLR);
+            land_RF = (Button) findViewById(R.id.land_buttonRF);
+            land_RR = (Button) findViewById(R.id.land_buttonRR);
+            land_LF.setOnClickListener(this);
+            land_LR.setOnClickListener(this);
+            land_RF.setOnClickListener(this);
+            land_RR.setOnClickListener(this);/**/
+            Toast.makeText(this,"Screen switched to Landscape mode",Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            vert_LF = (Button) findViewById(R.id.vert_buttonLF);
+            vert_LR = (Button) findViewById(R.id.vert_buttonLR);
+            vert_RF = (Button) findViewById(R.id.vert_buttonRF);
+            vert_RR = (Button) findViewById(R.id.vert_buttonRR);
+            vert_LF.setOnClickListener(this);
+            vert_LR.setOnClickListener(this);
+            vert_RF.setOnClickListener(this);
+            vert_RR.setOnClickListener(this);/**/
+            Toast.makeText(this,"Screen switched to Portrait mode", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.land_buttonLF:
+                //open alignment settings
+                Intent intent = new Intent(this, Session.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
